a0cacc65a9fab7ae8c71b691e118bc23a0be8a8c
yangentao <entaoyang@163.com>
2019-04-08 02:08:41 +0800

    Yog

---
+++ UtilApp/yog/src/main/java/dev/entao/yog/YogDir.kt
@@ -0,0 +1,80 @@
+package dev.entao.yog
+
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * Created by entaoyang@163.com on 2016-10-28.
+ */
+
+class YogDir(val logdir: File, val keepDays: Int = 30) : YogPrinter {
+
+
+    private var writer: BufferedWriter? = null
+    private var dateStr: String = ""
+
+    init {
+        if (!logdir.exists()) {
+            logdir.mkdirs()
+        }
+    }
+
+    val out: BufferedWriter?
+        get() {
+            val ff = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            val ds = ff.format(Date(System.currentTimeMillis()))
+            if (ds == dateStr) {
+                return writer
+            } else {
+                writer?.flush()
+                writer?.close()
+                writer = null
+            }
+            dateStr = ds
+            deleteOldLogs(logdir, keepDays)
+            try {
+                writer = BufferedWriter(FileWriter(File(logdir, "y$dateStr.log"), true), 20 * 1024)
+            } catch (ex: IOException) {
+                ex.printStackTrace()
+            }
+            return writer
+        }
+
+
+    private fun deleteOldLogs(logdir: File, days: Int) {
+        val ls = logdir.listFiles().filter { it.name.endsWith(".log") }.sortedByDescending { it.name }
+        val n = days + 1
+        if (n > 0 && ls.size > n) {
+            for (i in n until ls.size) {
+                ls[i].delete()
+            }
+        }
+
+    }
+
+    override fun flush() {
+        try {
+            out?.flush()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun printLine(level: LogLevel, msg: String) {
+        val w = out ?: return
+        val s = Yog.formatMsg(level, msg)
+        try {
+            w.write(s)
+            w.write("\n")
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        flush()
+
+    }
+
+}
