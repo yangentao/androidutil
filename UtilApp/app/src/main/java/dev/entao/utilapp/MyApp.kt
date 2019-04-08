5b6abd211123aa3c234e377cb726ea8f5d95c295
yangentao <entaoyang@163.com>
2019-04-09 01:41:57 +0800

    yapp

--- UtilApp/app/src/main/java/dev/entao/utilapp/MyApp.kt
+++ UtilApp/app/src/main/java/dev/entao/utilapp/MyApp.kt
@@ -1,5 +1,6 @@
 package dev.entao.utilapp
 
+import dev.entao.yog.logd
 import yet.ui.widget.TitleBar
 import yet.util.app.YetApp
 
@@ -8,5 +9,8 @@ class MyApp : YetApp() {
     override fun onCreate() {
         super.onCreate()
         TitleBar.TitleCenter = true
+        val s = "abc0123"
+        val a = s.map { it.toInt().toString(16) }.joinToString("")
+        logd(a)
     }
 }
